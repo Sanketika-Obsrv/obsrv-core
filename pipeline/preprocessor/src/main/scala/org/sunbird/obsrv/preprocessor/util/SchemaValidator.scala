97f2e91cef7cc0ece64dc9b2beddc28bb67ba6d5
Manjunath Davanam <manjunath@sanketika.in>
2024-08-26 18:09:13 +0530

    #OBS-I180: Schema Validator Changes to support Draft 12 version schema

--- pipeline/preprocessor/src/main/scala/org/sunbird/obsrv/preprocessor/util/SchemaValidator.scala
+++ pipeline/preprocessor/src/main/scala/org/sunbird/obsrv/preprocessor/util/SchemaValidator.scala
@@ -1,9 +1,8 @@
 package org.sunbird.obsrv.preprocessor.util
 
-import com.github.fge.jackson.JsonLoader
-import com.github.fge.jsonschema.core.exceptions.ProcessingException
-import com.github.fge.jsonschema.core.report.ProcessingReport
-import com.github.fge.jsonschema.main.{JsonSchema, JsonSchemaFactory}
+import com.fasterxml.jackson.databind.JsonNode
+import com.networknt.schema.SpecVersion.VersionFlag
+import com.networknt.schema._
 import org.slf4j.LoggerFactory
 import org.sunbird.obsrv.core.exception.ObsrvException
 import org.sunbird.obsrv.core.model.ErrorConstants
@@ -13,18 +12,29 @@ import org.sunbird.obsrv.model.DatasetModels.Dataset
 import java.io.IOException
 import scala.collection.mutable
 
-case class Schema(loadingURI: String, pointer: String)
+case class ValidationMsg(
+                          `type`: String,
+                          code: Option[String],
+                          message: String,
+                          instanceLocation: Option[String],
+                          property: Option[String],
+                          evaluationPath: Option[String],
+                          schemaLocation: Option[String],
+                          messageKey: Option[String],
+                          arguments: Option[Seq[String]],
+                          details: Option[Map[String, Any]]
+                        )
 
-case class Instance(pointer: String)
-
-case class ValidationMsg(level: String, schema: Schema, instance: Instance, domain: String, keyword: String, message: String, allowed: Option[String],
-                         found: Option[String], expected: Option[List[String]], unwanted: Option[List[String]], required: Option[List[String]], missing: Option[List[String]])
 
 class SchemaValidator() extends java.io.Serializable {
 
   private val serialVersionUID = 8780940932759659175L
   private[this] val logger = LoggerFactory.getLogger(classOf[SchemaValidator])
   private[this] val schemaMap = mutable.Map[String, (JsonSchema, Boolean)]()
+  // This creates a schema factory that will use Draft 2020-12 as the default if $schema is not specified
+  // in the schema data. If $schema is specified in the schema data then that schema dialect will be used
+  // instead and this version is ignored.
+  private[this] val schemaFactory = JsonSchemaFactory.getInstance(VersionFlag.V202012)
 
   def loadDataSchemas(datasets: List[Dataset]): Unit = {
     datasets.foreach(dataset => {
@@ -48,35 +58,54 @@ class SchemaValidator() extends java.io.Serializable {
     }
   }
 
-  private def loadJsonSchema(datasetId: String, jsonSchemaStr: String) = {
-    val schemaFactory = JsonSchemaFactory.byDefault
+  private def loadJsonSchema(datasetId: String, jsonSchemaStr: String): Unit = {
     try {
-      val jsonSchema = schemaFactory.getJsonSchema(JsonLoader.fromString(jsonSchemaStr))
-      jsonSchema.validate(JSONUtil.convertValue(Map("pqr" -> "value"))) // Test validate to check if Schema is valid
+      val schemaValidatorsConfig = SchemaValidatorsConfig.builder().build()
+      val jsonSchema = schemaFactory.getSchema(jsonSchemaStr, schemaValidatorsConfig)
+      jsonSchema.validate(convertToJsonNode(Map())) // Test validate to check if Schema is valid
       schemaMap.put(datasetId, (jsonSchema, true))
     } catch {
       case ex: Exception =>
-        logger.error(s"SchemaValidator:loadJsonSchema() - Unable to parse the schema json for dataset: $datasetId", ex)
+        println(s"SchemaValidator:loadJsonSchema() - Unable to parse the schema json for dataset: $datasetId", ex)
         throw new ObsrvException(ErrorConstants.INVALID_JSON_SCHEMA)
     }
   }
 
   def schemaFileExists(dataset: Dataset): Boolean = {
-      schemaMap.get(dataset.id).map(f => f._2).orElse(Some(false)).get
+    //println("schemaMap " + schemaMap)
+    schemaMap.get(dataset.id).map(f => f._2).orElse(Some(false)).get
   }
 
   @throws[IOException]
-  @throws[ProcessingException]
-  def validate(datasetId: String, event: Map[String, AnyRef]): ProcessingReport = {
-    schemaMap(datasetId)._1.validate(JSONUtil.convertValue(event))
+  def validate(datasetId: String, event: Map[String, AnyRef]): Set[ValidationMessage] = {
+
+    import scala.collection.JavaConverters._
+    val schema = schemaMap.getOrElse(datasetId, throw new ObsrvException(ErrorConstants.JSON_SCHEMA_NOT_FOUND))._1
+    val messages = schema.validate(convertToJsonNode(event)).asScala.toSet
+    messages.foreach { message =>
+      println(s"Type: ${message.getType}")
+      println(s"Code: ${message.getCode}")
+      println(s"Message: ${message.getMessage}")
+      println(s"Instance Location: ${message.getInstanceLocation}")
+      println(s"Property: ${message.getProperty}")
+      println(s"Evaluation Path: ${message.getEvaluationPath}")
+      println(s"Schema Location: ${message.getSchemaLocation}")
+      println(s"Message Key: ${message.getMessageKey}")
+      println(s"Arguments: ${JSONUtil.serialize(message.getArguments)}")
+      println(s"Details: ${JSONUtil.serialize(message.getDetails)}")
+      println("-" * 40) // Separator for readability
+    }
+    messages
   }
 
-  def getValidationMessages(report: ProcessingReport): List[ValidationMsg] = {
-    val buffer = mutable.Buffer[ValidationMsg]()
-    report.forEach(processingMsg => {
-      buffer.append(JSONUtil.deserialize[ValidationMsg](JSONUtil.serialize(processingMsg.asJson())))
-    })
-    buffer.toList
+  def getValidationMessages(validationMessages: Set[ValidationMessage]): List[ValidationMsg] = {
+    validationMessages.map { validationMessage =>
+      JSONUtil.deserialize[ValidationMsg](JSONUtil.serialize(validationMessage))
+    }.toList
+  }
+
+  private def convertToJsonNode(data: Map[String, AnyRef]): JsonNode = {
+    JSONUtil.convertValue(data)
   }
 
 }

bd57989834a4ec82f1420338297912f83eed8639
Manjunath DR <manjunath@sanketika.in>
2025-08-07 12:32:50 +0530

    Release 2.0.0 | Software Upgrade (#117)
    * Upgrade flink from 1.17 to 1.20 (#111)
    
    * Upgrade flink from 1.17 to 1.20
    
    * Upgrade flink from 1.17 to 1.20
    - Fixed failing test cases
    - Upgraded FlinkUtil by removing deprecated code
    
    * Removed the legacy unreachable scala tool
    
    * Updated the mvn clean install command
    
    * Updated the mvn clean install command
    
    * Updated the github actions
    
    * Updated the github actions and docker file
    
    * Docker image changes to support 1.20 flink version
    
    ---------
    
    Co-authored-by: Santhosh <santhosh@sanketika.in>
    Co-authored-by: Manjunath <manju@Manjunaths-MacBook-Air.local>

--- framework/src/main/scala/org/sunbird/obsrv/core/util/FlinkUtil.scala
+++ framework/src/main/scala/org/sunbird/obsrv/core/util/FlinkUtil.scala
@@ -1,37 +1,39 @@
 package org.sunbird.obsrv.core.util
 
-import org.apache.flink.api.common.restartstrategy.RestartStrategies
-import org.apache.flink.runtime.state.hashmap.HashMapStateBackend
-import org.apache.flink.streaming.api.CheckpointingMode
-import org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup
-import org.apache.flink.streaming.api.environment.{CheckpointConfig, StreamExecutionEnvironment}
+import org.apache.flink.configuration._
+import org.apache.flink.core.execution.CheckpointingMode
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import org.sunbird.obsrv.core.streaming.BaseJobConfig
+
+import java.time.Duration
 
 object FlinkUtil {
 
   def getExecutionContext(config: BaseJobConfig[_]): StreamExecutionEnvironment = {
-    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-    env.getConfig.setUseSnapshotCompression(config.enableCompressedCheckpointing)
-    env.enableCheckpointing(config.checkpointingInterval)
 
-    /**
-      * Use Blob storage as distributed state backend if enabled
-      */
+    val configuration = new Configuration()
+    configuration.set(RestartStrategyOptions.RESTART_STRATEGY, "fixed-delay")
+    configuration.set[Integer](RestartStrategyOptions.RESTART_STRATEGY_FIXED_DELAY_ATTEMPTS, config.restartAttempts)
+    configuration.set(RestartStrategyOptions.RESTART_STRATEGY_FIXED_DELAY_DELAY, Duration.ofMillis(config.delayBetweenAttempts))
+    configuration.set(CheckpointingOptions.CHECKPOINTING_INTERVAL, Duration.ofMillis(config.checkpointingInterval))
+    configuration.set[java.lang.Boolean](ExecutionOptions.SNAPSHOT_COMPRESSION, config.enableCompressedCheckpointing)
+
     // $COVERAGE-OFF$ Disabling scoverage as the below code can only be invoked with a cloud blob store config
+    /* Use Blob storage as distributed state backend if enabled */
     config.enableDistributedCheckpointing match {
       case Some(true) => {
-        env.setStateBackend(new HashMapStateBackend())
-        val checkpointConfig: CheckpointConfig = env.getCheckpointConfig
-        checkpointConfig.setExternalizedCheckpointCleanup(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
-        checkpointConfig.setMinPauseBetweenCheckpoints(config.checkpointingPauseSeconds)
-        checkpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
-        checkpointConfig.setCheckpointStorage(s"${config.checkpointingBaseUrl.getOrElse("")}/${config.jobName}")
+        configuration.set(StateBackendOptions.STATE_BACKEND, "hashmap")
+        configuration.set(CheckpointingOptions.CHECKPOINTING_CONSISTENCY_MODE, CheckpointingMode.EXACTLY_ONCE)
+        configuration.set(CheckpointingOptions.MIN_PAUSE_BETWEEN_CHECKPOINTS, Duration.ofSeconds(config.checkpointingPauseSeconds))
+        configuration.set(CheckpointingOptions.EXTERNALIZED_CHECKPOINT_RETENTION, ExternalizedCheckpointRetention.RETAIN_ON_CANCELLATION)
+        configuration.set(CheckpointingOptions.CHECKPOINT_STORAGE, "filesystem")
+        configuration.set(CheckpointingOptions.CHECKPOINTS_DIRECTORY, s"${config.checkpointingBaseUrl.getOrElse("")}/${config.jobName}")
       }
       case _ => // Do nothing
     }
     // $COVERAGE-ON$
-
-    env.setRestartStrategy(RestartStrategies.fixedDelayRestart(config.restartAttempts, config.delayBetweenAttempts))
+    val env = StreamExecutionEnvironment.getExecutionEnvironment(configuration)
+    print("Flink Config:", env.getConfiguration)
     env
   }
 }
